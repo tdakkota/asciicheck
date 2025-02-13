daaab23be434a9a080a5fbaf755c4ae5743f2cfb
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-02-13 14:18:29 +0200

    refactor: remove usage of deprecated ast.Object

--- testdata/variable_name.go
+++ testdata/variable_name.go
@@ -3,7 +3,7 @@ package testdata
 import "fmt"
 
 func f() {
-	var tеstVar int // want `identifier "tеstVar" contain non-ASCII character: U\+0435 'е'`
-	tеstVar = 0
-	fmt.Println(tеstVar)
+	var tеstVar int      // want `identifier "tеstVar" contain non-ASCII character: U\+0435 'е'`
+	tеstVar = 0          // want `identifier "tеstVar" contain non-ASCII character: U\+0435 'е'`
+	fmt.Println(tеstVar) // want `identifier "tеstVar" contain non-ASCII character: U\+0435 'е'`
 }
